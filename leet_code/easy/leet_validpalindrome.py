975ad3f74541e2fb40afaf69499c6069d11df4d5
Ng Wei Sheng <weisheng_rules@hotmail.com>
2019-08-21 03:22:41 +0800

    refactored valid palindrome problem

--- leet_code/easy/leet_validpalindrome.py
+++ leet_code/easy/leet_validpalindrome.py
@@ -6,11 +6,13 @@ class Solution(object):
         """
 
         s = [S for S in s.lower() if S.isalnum()]
+        
+        # redundant condition statements that i added because i got isalnum() confused with isalpha()
 
-        if len(s) == 2 and len(set(s)) != 1:
-            return False
-        if not s:
-            return True
+        # if len(s) == 2 and len(set(s)) != 1:
+        #     return False
+        # if not s:
+        #     return True
         
         return s == s[::-1]
             
